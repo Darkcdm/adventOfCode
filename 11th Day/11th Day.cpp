b77a731e99bf3552c7820c9cf28ef46716764507
Darkcdm <darksniped@gmail.com>
2023-02-03 14:56:59 +0100

    mid way

--- 11th Day/11th Day.cpp
+++ 11th Day/11th Day.cpp
@@ -2,10 +2,75 @@
 //
 
 #include <iostream>
+#include <functional>
+
+using namespace std;
+
+class Monkey {
+public:
+    std::function<void()> m_function;
+
+    Monkey(std::function<void()> getm_function) {
+        m_function = getm_function;
+    }
+
+};
+
+int invoke(int x, int y,
+    function<int(int, int)> func)
+{
+    return func(x, y);
+}
+
+/*
+Monkey 0:
+    Starting items : 79, 98
+    Operation : new = old * 19
+    Test : divisible by 23
+    If true : throw to monkey 2
+    If false : throw to monkey 3
+
+Monkey 1 :
+    Starting items : 54, 65, 75, 74
+    Operation : new = old + 6
+    Test : divisible by 19
+    If true : throw to monkey 2
+    If false : throw to monkey 0
+
+    Monkey 2 :
+    Starting items : 79, 60, 97
+    Operation : new = old * old
+    Test : divisible by 13
+    If true : throw to monkey 1
+    If false : throw to monkey 3
+
+    Monkey 3 :
+    Starting items : 74
+    Operation : new = old + 3
+    Test : divisible by 17
+    If true : throw to monkey 0
+    If false : throw to monkey 1
+*/
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::function<void()> m_function = [this]() { std::cout << "test\n";   };
+    Monkey test(m_function);
+
+
+
+        cout << "Addition of 20 and 10 is ";
+        int k = invoke(20, 10,
+            [](int x,
+                int y) -> int {
+                    return x + y;
+            });
+
+        cout << k << '\n';
+
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
