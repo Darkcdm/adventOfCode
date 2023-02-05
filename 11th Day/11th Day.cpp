927a312896a7b2c754fbdefe31a07960374d2874
Darkcdm <darksniped@gmail.com>
2023-02-05 21:08:44 +0100

    Update 11th Day.cpp

--- 11th Day/11th Day.cpp
+++ 11th Day/11th Day.cpp
@@ -3,74 +3,68 @@
 
 #include <iostream>
 #include <functional>
+#include <cstdlib>
+#include <fstream>
+#include <string>
+#include <cmath>
 
 using namespace std;
 
-class Monkey {
+class Test {
 public:
-    std::function<void()> m_function;
-
-    Monkey(std::function<void()> getm_function) {
-        m_function = getm_function;
-    }
-
+    int testValue;
+    int truy;
+    int falsy;
 };
 
-int invoke(int x, int y,
-    function<int(int, int)> func)
-{
-    return func(x, y);
+class Monkey {
+public:
+    int *items;
+    Test test;
+};
+
+void parseInput(Monkey **monkeys) {
+    ifstream input("input.txt");
+    string line;
+
+    for (int count = 0; count < 7; count++) {
+        getline(input, line);
+
+        switch (count) {
+        case 0:
+            //Monkey name
+            break;
+        case 1:
+            line.substr()
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        
+        default:
+            return;
+        }
+        
+    }
+
+    input.close();
 }
-
-/*
-Monkey 0:
-    Starting items : 79, 98
-    Operation : new = old * 19
-    Test : divisible by 23
-    If true : throw to monkey 2
-    If false : throw to monkey 3
-
-Monkey 1 :
-    Starting items : 54, 65, 75, 74
-    Operation : new = old + 6
-    Test : divisible by 19
-    If true : throw to monkey 2
-    If false : throw to monkey 0
-
-    Monkey 2 :
-    Starting items : 79, 60, 97
-    Operation : new = old * old
-    Test : divisible by 13
-    If true : throw to monkey 1
-    If false : throw to monkey 3
-
-    Monkey 3 :
-    Starting items : 74
-    Operation : new = old + 3
-    Test : divisible by 17
-    If true : throw to monkey 0
-    If false : throw to monkey 1
-*/
-
-
 
 int main()
 {
-    std::function<void()> m_function = [this]() { std::cout << "test\n";   };
-    Monkey test(m_function);
+    Monkey* monkeys = (Monkey*)calloc(10, sizeof(Monkey));
 
 
 
-        cout << "Addition of 20 and 10 is ";
-        int k = invoke(20, 10,
-            [](int x,
-                int y) -> int {
-                    return x + y;
-            });
-
-        cout << k << '\n';
+    parseInput(&monkeys);
 
 
+    free(monkeys);
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
