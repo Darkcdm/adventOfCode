ac3a39e954419bd0feef73eecbea706dca9105f0
Darkcdm <darksniped@gmail.com>
2023-05-01 13:18:26 +0200

    Update 14th Day.cpp

--- 14th Day/14th Day.cpp
+++ 14th Day/14th Day.cpp
@@ -10,121 +10,249 @@
 using namespace std;
 
 struct Cell {
-    int x;
-    int y;
-    int trueX;
-    int trueY;
-    
-    bool occupied;
-    bool solid;
-    char symbol;
+	int x;
+	int y;
+	int trueX;
+	int trueY;
 
-    Cell(int x, int y) {
-        this->x = x;
-        this->y = y;
+	bool occupied;
+	bool solid;
+	char symbol;
+	bool source;
 
-        occupied = false;
-        solid = false;
-        symbol = '.';
-    }
+	Cell(int x, int y, int trueX, int trueY) {
+		this->x = x;
+		this->y = y;
+		this->trueX = trueX;
+		this->trueY = trueY;
+
+		occupied = false;
+		solid = false;
+		source = false;
+		symbol = '.';
+	}
 };
 
 //returns Top Left and Bottom Right Coords
 vector<int> parseDimension() {
-    vector<int> results;
+	vector<int> results;
 
-    ifstream input;
-    input.open("input.txt");
+	ifstream input;
+	input.open("input.txt");
 
-    string line;
+	string line;
 
-    while (getline(input, line)) {
-        int leftIndex = 0;
-        int rightIndex = 0;
+	while (getline(input, line)) {
+		int leftIndex = 0;
+		int rightIndex = 0;
 
-        while(rightIndex != line.size()) {
-            leftIndex = rightIndex;
-            rightIndex = line.find(' ', leftIndex+1);
-            
-            if (rightIndex == -1) {
-                rightIndex = line.size();
-            }
+		while (rightIndex != line.size()) {
+			leftIndex = rightIndex;
+			rightIndex = line.find(' ', leftIndex + 1);
 
-            string coords = line.substr(leftIndex, rightIndex - leftIndex);
+			if (rightIndex == -1) {
+				rightIndex = line.size();
+			}
 
-            if (coords[1] == '-') {
-                leftIndex = rightIndex;
-                rightIndex = line.find(' ', leftIndex + 1);
+			string coords = line.substr(leftIndex, rightIndex - leftIndex);
 
-                if (rightIndex == -1) {
-                    rightIndex = line.size();
-                }
+			if (coords[1] == '-') {
+				leftIndex = rightIndex;
+				rightIndex = line.find(' ', leftIndex + 1);
 
-                coords = line.substr(leftIndex, rightIndex - leftIndex);
-            }
+				if (rightIndex == -1) {
+					rightIndex = line.size();
+				}
 
-            string strX = coords.substr(0, coords.find(','));
-            string strY = coords.substr(coords.find(',')+1);
+				coords = line.substr(leftIndex, rightIndex - leftIndex);
+			}
 
-            int x = stoi(strX);
-            int y = stoi(strY);
-      
-            if (results.empty()) {
-                results.push_back(x);
-                results.push_back(y);
-                results.push_back(x);
-                results.push_back(y);
-            }
+			string strX = coords.substr(0, coords.find(','));
+			string strY = coords.substr(coords.find(',') + 1);
 
-            if (x < results[0]) {
-                results[0] = x;
-            }
-            if (y < results[1]) {
-                results[1] = y;
-            }
-            if (x > results[2]) {
-                results[2] = x;
-            }
-            if (y > results[3]) {
-                results[3] = y;
-            }
+			int x = stoi(strX);
+			int y = stoi(strY);
+
+			if (results.empty()) {
+				results.push_back(x);
+				results.push_back(y);
+				results.push_back(x);
+				results.push_back(y);
+			}
+
+			if (x < results[0]) {
+				results[0] = x;
+			}
+			if (y < results[1]) {
+				results[1] = y;
+			}
+			if (x > results[2]) {
+				results[2] = x;
+			}
+			if (y > results[3]) {
+				results[3] = y;
+			}
 
 
-        }
-    }
+		}
+	}
 
-    input.close();
+	input.close();
 
-    return results;
+	return results;
 }
 
 void printCave(vector<vector<Cell>> cave) {
-
+	for (int y = 0; y < cave.size(); y++) {
+		for (int x = 0; x < cave[y].size(); x++) {
+			cout << cave[y][x].symbol;
+		}
+		cout << endl;
+	}
 }
 
 vector<vector<Cell>> createCave(int depth, int width, int leftX) {
-    vector<vector<Cell>> result;
+	vector<vector<Cell>> result;
 
-    for (int y = 0; y < depth; y++) {
-        for (int x = 0; x < width; x++) {
+	result.resize(depth);
 
-        }
-    }
+	//create the space
+	for (int y = 0; y < depth; y++) {
+		for (int x = 0; x < width; x++) {
+			Cell newCell(x, y, leftX + x, y);
+			//place source
+			if (leftX + x == 500 && y == 0) {
+				newCell.source = true;
+				newCell.symbol = '+';
+			}
 
-    return result;
+			result[y].push_back(newCell);
+		}
+	}
+
+
+
+
+
+
+
+	ifstream input;
+	string line;
+	input.open("input.txt");
+
+
+
+
+
+
+	//parse Lines
+	while (getline(input, line)) {
+		int leftIndex = 0;
+		int rightIndex = 0;
+
+		vector<int> points;
+		points.resize(4);
+
+		leftIndex = rightIndex;
+		rightIndex = line.find(' ', leftIndex + 1);
+
+		//fix shit if you're at the end
+		if (rightIndex == -1) {
+			rightIndex = line.size();
+		}
+
+		string coords = line.substr(leftIndex, rightIndex - leftIndex);
+
+		//if you're in the middle then cross out the arrow
+		if (coords[1] == '-') {
+			leftIndex = rightIndex;
+			rightIndex = line.find(' ', leftIndex + 1);
+
+			if (rightIndex == -1) {
+				rightIndex = line.size();
+			}
+
+			coords = line.substr(leftIndex, rightIndex - leftIndex);
+		}
+
+		//parse the strings into numbers
+		string strX = coords.substr(0, coords.find(','));
+		string strY = coords.substr(coords.find(',') + 1);
+
+		points[2] = stoi(strX);
+		points[3] = stoi(strY);
+
+
+
+
+
+
+		//go untill you reach the end of the file
+		while (rightIndex != line.size()) {
+			points[0] = points[2];
+			points[1] = points[3];
+
+			leftIndex = rightIndex;
+			rightIndex = line.find(' ', leftIndex + 1);
+
+			//fix shit if you're at the end
+			if (rightIndex == -1) {
+				rightIndex = line.size();
+			}
+
+			coords = line.substr(leftIndex, rightIndex - leftIndex);
+
+			//if you're in the middle then cross out the arrow
+			if (coords[1] == '-') {
+				leftIndex = rightIndex;
+				rightIndex = line.find(' ', leftIndex + 1);
+
+				if (rightIndex == -1) {
+					rightIndex = line.size();
+				}
+
+				coords = line.substr(leftIndex, rightIndex - leftIndex);
+			}
+
+			//parse the strings into numbers
+			strX = coords.substr(0, coords.find(','));
+			strY = coords.substr(coords.find(',') + 1);
+			points[2] = stoi(strX);
+			points[3] = stoi(strY);
+
+			//do something with parsed values
+			//...
+
+			vector<int> paintVector = {points[0]+points[2], points[1]+points[3]};
+
+			for (int x = 0; x < paintVector[0]; x++) {
+				result[points[1]][points[0] + x].solid = true;
+				result[points[1]][points[0] + x].symbol = '#';
+
+			}
+			for (int y = 0; y < paintVector[1]; y++) {
+				result[points[1] + y][points[0]].solid = true;
+				result[points[1] + y][points[0]].symbol = '#';
+			}
+		}
+	}
+
+	input.close();
+	return result;
+
 }
 
-int main(){
-    vector<int> dimensions = parseDimension();
+int main() {
+	vector<int> dimensions = parseDimension();
 
-    vector<vector<Cell>> cave = createCave(dimensions[3], dimensions[2] - dimensions[0] + 2, dimensions[2]);
+	vector<vector<Cell>> cave = createCave(dimensions[3], dimensions[2] - dimensions[0] + 2, dimensions[0] - 1);
 
-    cout << dimensions[0] << ", ";
-    cout << dimensions[1] << ", ";
+	cout << dimensions[0] << ", ";
+	cout << dimensions[1] << ", ";
 
-    cout << dimensions[2] << ", ";
-    cout << dimensions[3] << ", ";
+	cout << dimensions[2] << ", ";
+	cout << dimensions[3] << ", ";
+	cout << endl;
 
-
-    printCave(cave);
+	printCave(cave);
 }
