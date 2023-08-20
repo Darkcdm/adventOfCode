fcb62b6220af1f18659fc53654357d6a5ff1907c
Darkcdm <darksniped@gmail.com>
2023-08-20 17:53:16 +0200

    UPDATE

--- 16th Day/16th Day.cpp
+++ 16th Day/16th Day.cpp
@@ -2,12 +2,14 @@
 #include <vector>
 #include <fstream>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
 string inputName;
 
 struct Valve{
+	int id;
 	string name;
 	int flow;
 	vector <int> routes;
@@ -24,7 +26,18 @@ void setInputs(bool setCheck) {
 	}
 }
 
-void sortInputFile() {
+
+int findMatchingIndex(const std::vector<std::string>& strings, const std::string& target) {
+	for (int i = 0; i < strings.size(); ++i) {
+		if (strings[i] == target) {
+			return i;  // Return the index of the first match
+		}
+	}
+
+	return -1;  // Return -1 if no match is found
+}
+
+vector <Valve> sortInputFile() {
 	fstream input;
 	input.open(inputName);
 
@@ -78,26 +91,117 @@ void sortInputFile() {
 		}
 	} while (check);
 
-	for (size_t i = 0; i < lines.size(); i++) {
-		cout << lines[i] << endl;
-	}
-
 	vector <string> nameList;
+	vector <int> flowList;
+	vector <vector<string>> routeList;
 
 	for (size_t i = 0; i < lines.size(); i++) {
 		nameList.push_back(lines[i].substr(6, 2));
+		flowList.push_back(stoi(lines[i].substr(lines[i].find("= ") + 2, lines[i].find("= ") + 2 - lines[i].find(';'))));
+	
+		vector<string> lineRouteList;
+
+		string routeString = lines[i].substr(lines[i].find("to valves") + 9, lines[i].find("to valves") + 9 -  lines[i].size());
+		
+		string selected;
+		int routeStringIndex = 0;
+
+		char cursor;
+		for (cursor = routeString[routeStringIndex]; routeStringIndex < routeString.size(); cursor = routeString[++routeStringIndex]) {
+			if (cursor != '\n' && cursor != ' ' && cursor != ',') {
+				selected.push_back(cursor);
+			}
+			
+			if (cursor == '\n' || cursor == ',') {
+				lineRouteList.push_back(selected);
+				selected.clear();
+			}
+
+		} 
+		//selected.push_back(cursor);
+		lineRouteList.push_back(selected);
+		selected.clear();
+
+		routeList.push_back(lineRouteList);
 	}
+
+
+	vector<Valve> result;
 
 	for (size_t i = 0; i < lines.size(); i++) {
 		
+		Valve valve;
+
+		valve.id = i;
+		valve.flow = flowList[i];
+		valve.name = nameList[i];
+
+		for (size_t iR = 0; iR < routeList[i].size(); iR++) {
+			valve.routes.push_back(findMatchingIndex(nameList , routeList[i][iR]));
+		}
+
+		
+		cout << valve.id << ';' << valve.name << ';' << valve.flow << "f;" ;
+		for (size_t iS = 0; iS < valve.routes.size(); iS++) {
+			cout << valve.routes[iS] << ",";
+		}
+		cout << ';' << endl;
+	
+		result.push_back(valve);
 	}
 
+	
+	return result;
+}
+
+vector<Valve> generateTestInput() {
+	vector<Valve> result;
+  for (int i = 0; i < 10; i++) {
+		Valve valve;
+		valve.id = i;
+		valve.flow = rand() % 10 + 1;
+		valve.name = "V" + to_string(i);
+		valve.routes = {};
+		result.push_back(valve);
+	}
+	for (Valve v : result) {
+		for (int i = 0; i < rand() % 3 + 1; i++) {
+			v.routes.push_back(rand() % 10);
+		}
+	}
+	return result;
+}
+
+// Maximal weighted independent set
+int mwis (vector<Valve> & graph, Valve & source, int remainingMinutes, int maxFlow, vector<Valve> & visited_path) {
+	//TODO Add DP
+	if (remainingMinutes == 0) {
+		return maxFlow;
+	}
+	std::vector<Valve> neighbors = {};
+	for (int i = 0; i < source.routes.size(); i++) {
+		neighbors.push_back(graph[source.routes[i]]);
+	}
+	for (int i = 0; i < neighbors.size(); i++) {
+		int temp = mwis(graph, neighbors[i], remainingMinutes - 1, maxFlow, visited_path);
+		if (temp > maxFlow) {
+			remainingMinutes--;
+			maxFlow = temp;
+		}
+	}
+	visited_path.push_back(source);
+	return maxFlow + source.flow*remainingMinutes;
 }
 
 int main(){
-	setInputs(1);
+	setInputs(0);
 
-	sortInputFile();
+	//vector<Valve> testGraph = generateTestInput();
+
+	vector <Valve> testGraph = sortInputFile();
+
+	vector<Valve> visited_path = {};
+	cout << mwis(testGraph, testGraph[0], 30, -1, visited_path) << endl;
 
 
 	return 0;
