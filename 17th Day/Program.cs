3e29a20b341e6f9549a1a54d8fc3999cfff3b304
Darkcdm <darksniped@gmail.com>
2023-08-29 16:38:31 +0200

    created 17th Day project

---
+++ 17th Day/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AoC17 {
+    class Program {
+        static void Main() {
+            Console.WriteLine("Starting the program");
+
+            var result = -1; 
+
+            Console.WriteLine("Result is: " + result);
+            Console.WriteLine("Ending the program");
+        }
+    }
+}
