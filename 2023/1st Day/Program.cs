ecf69c8f8e0b71a5ce391b63902c6ec6c50ceeba
Darkcdm <darksniped@gmail.com>
2023-12-01 20:51:43 +0100

    Second part of the first day is done

--- 2023/1st Day/Program.cs
+++ 2023/1st Day/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System;
+using System.Text.RegularExpressions;
+using System.Threading;
 
 namespace _1st_Day {
     class Program {
@@ -16,7 +18,7 @@ namespace _1st_Day {
 
     public class LineReader {
         string inputFile;
-        List<string> filteredInput = new List<string>();
+        List<List<string>> filteredInput = new List<List<string>>();
         List<int> transformedInput = new List<int>();
         
         public LineReader(string inputFile) {
@@ -30,40 +32,93 @@ namespace _1st_Day {
                     // Read lines until the end of the file is reached
                     while (!reader.EndOfStream) {
                         // Read the current line
-                        string line = reader.ReadLine();
+                        string line = "";
+                        line = reader.ReadLine();
 
-                        string foundChars = "";
+                        string reverseLine = reverse(line);
 
-                        // Process the line
-                        //checking from front
-                        for(int i = 0; i < line.Length; i++) {
-                            if (line[i] == '0' || line[i] == '1' || line[i] == '2' || line[i] == '3' || line[i] == '4' || line[i] == '5' || line[i] == '6' || line[i] == '7' || line[i] == '8' || line[i] == '9') {
-  
-                                foundChars = foundChars + line[i];
-                                break;
-                            }
-                        }
-                        //checking from the back
-                        for(int i = line.Length - 1 ; i >= 0; i--) {
-                            if (line[i] == '0' || line[i] == '1' || line[i] == '2' || line[i] == '3' || line[i] == '4' || line[i] == '5' || line[i] == '6' || line[i] == '7' || line[i] == '8' || line[i] == '9') {
-     
-                                foundChars = foundChars + line[i];
-                                break;
-                            }
-                        }
-                        filteredInput.Add(foundChars);
+                        string pattern = "(one|two|three|four|five|six|seven|eight|nine|[1-9])";
+                        string reversePattern = "(eno|owt|eerht|ruof|evif|xis|neves|thgie|enin|[1-9])";
+
+
+                        // Create a Regex object
+                        Regex regex = new Regex(pattern);
+                        Regex reverseRegex = new Regex(reversePattern);
+
+                        // Use the Matches method to find all matches in the sentence
+                        MatchCollection matches = regex.Matches(line);
+                        MatchCollection reverseMatches = reverseRegex.Matches(reverseLine);
+                        
+                        List<String> pair = new List<String>();
+                        pair.Add(matches.First().Value);
+                        pair.Add(reverse(reverseMatches.First().Value));
+
+                        filteredInput.Add(pair);
+         
                     }
+
+
                 }
-            } else {
-                Console.WriteLine("File not found.");
+                } else {
+                    Console.WriteLine("File not found.");
             }
+        }
+        string reverse(String text) {
+            string result = "";
+            for (int i = text.Length - 1; i >= 0; i--) {
+                result += text[i];
+            }
+            return result;
         }
 
         void transformInput() {
             for (int i = 0; i < filteredInput.Count; i++) {
 
+                String numberStr = "";
 
-                transformedInput.Add(int.Parse(filteredInput[i]));
+                foreach(String value in filteredInput[i]) {
+                    if (IsNumeric(value)) {
+                        numberStr += value;
+                    } else {
+                        switch(value) {
+                            case "one":
+                                numberStr += "1";
+                                break;
+                            case "two":
+                                numberStr += "2";
+                                break;
+                            case "three":
+                                numberStr += "3";
+                                break;
+                            case "four":
+                                numberStr += "4";
+                                break;
+                            case "five":
+                                numberStr += "5";
+                                break;
+                            case "six":
+                                numberStr += "6";
+                                break;
+                            case "seven":
+                                numberStr += "7";
+                                break;
+                            case "eight":
+                                numberStr += "8";
+                                break;
+                            case "nine":
+                                numberStr += "9";
+                                break;
+                            default:
+                                
+                                Console.WriteLine("FUCK");
+                            break;
+                        }
+                    }
+                }
+
+                Console.WriteLine(int.Parse(numberStr) + "=" + numberStr);
+
+                transformedInput.Add(int.Parse(numberStr));
             }
         }
 
@@ -72,6 +127,11 @@ namespace _1st_Day {
             transformInput();
 
             return transformedInput.AsQueryable().Sum();
+        }
+
+        static bool IsNumeric(string input) {
+            // Try to parse the string as a number
+            return !string.IsNullOrWhiteSpace(input) && double.TryParse(input, out _);
         }
 
     }
